6d8b756074c382e8dce67c1fa163a5ab930e18a3
Ricky Laney <rlaneyjr@gmail.com>
2020-07-20 14:13:21 -0400

    Added ipdb and scratch stuff

--- scratch.py
+++ scratch.py
@@ -23,7 +23,6 @@ print(ne_list)
 
 from nettopo.core.data import VLANData
 vlan = VLANData(22, 'flynet')
-
 vlan._as_dict()
 vlan.show()
 vlan._as_dict().items()
@@ -43,3 +42,56 @@ net.set_discover_maxdepth(100)
 net.discover_network('10.0.0.1', True)
 
 
+import ipdb
+from nettopo.core.constants import *
+from nettopo.core.util import *
+def get_cdp_neighbors(switch):
+    """ Get a list of CDP neighbors.
+    Returns a list of LinkData's.
+    Will always return an array.
+    """
+    # get list of CDP neighbors
+    neighbors = []
+    switch.cache.cdp = switch.snmp.get_bulk(OID.CDP)
+    ipdb.set_trace()
+    if not switch.cache.cdp:
+        print('No CDP Neighbors Found.')
+        return []
+    for row in switch.cache.cdp:
+        for oid, val in row:
+            oid = str(oid)
+            # process only if this row is a CDP_DEVID
+            if oid.startswith(OID.CDP_DEVID):
+                continue
+            t = oid.split('.')
+            ifidx = t[14]
+            ifidx2 = t[15]
+            idx = f".{ifidx}.{ifidx2}"
+            # get remote IP
+            rip = lookup_table(switch.cache.cdp, f"{OID.CDP_IPADDR}{idx}")
+            rip = ip_2_str(rip)
+            # get local port
+            lport = switch.get_ifname(ifidx)
+            # get remote port
+            rport = lookup_table(switch.cache.cdp, f"{OID.CDP_DEVPORT}{idx}")
+            rport = normalize_port(rport)
+            # get remote platform
+            rplat = lookup_table(switch.cache.cdp, f"{OID.CDP_DEVPLAT}{idx}")
+            # get IOS version
+            rios = lookup_table(switch.cache.cdp, f"{OID.CDP_IOS}{idx}")
+            if rios:
+                try:
+                    rios = binascii.unhexlify(rios[2:])
+                except:
+                    pass
+                rios = format_ios_ver(rios)
+                link = switch.get_link(ifidx)
+                link.remote_name = val.prettyPrint()
+                link.remote_ip = rip
+                link.discovered_proto = 'cdp'
+                link.local_port = lport
+                link.remote_port = rport
+                link.remote_plat = rplat
+                link.remote_ios = rios
+                neighbors.append(link)
+        return neighbors
