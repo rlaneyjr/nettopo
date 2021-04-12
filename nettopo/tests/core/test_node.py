dc98eeb3b4ac404d13a3bbbb99bc37bee77b37d1
Ricky Laney <rlaneyjr@gmail.com>
2021-04-11 22:06:50 -0400

    Added tests

---
+++ nettopo/tests/core/test_node.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# vim: noai:et:tw=80:ts=4:ss=4:sts=4:sw=4:ft=python
+
+'''
+Title:              test_node.py
+Description:        Test Node Functionality
+Author:             Ricky Laney
+Version:            0.1.0
+'''
+from nettopo.core.node import Node, Nodes
+import unittest
+
+
+class TestNode(unittest.TestCase):
+    def test_node()
+
+
+if __name__ == '__main__':
+    unittest.main()
